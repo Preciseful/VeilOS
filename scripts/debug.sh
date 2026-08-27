7792be3edacadbb167ae2dff0d953f5ab94ab8b0
Developful <preciseful1@gmail.com>
2026-08-27 08:00:13 +0300

    better

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -12,7 +12,7 @@ tmux split-window -v -t "veil-debug:debug.0"
 tmux split-window -h -t "veil-debug:debug.1"
 
 tmux send-keys -t "veil-debug:debug.1" \
-    'aarch64-none-elf-gdb -q build/kernel8.elf' C-m
+    'clear; until aarch64-none-elf-gdb -q -batch -ex "target remote :3333" -ex "monitor halt" build/kernel8.elf >/dev/null 2>&1; do sleep 1; done; aarch64-none-elf-gdb -q build/kernel8.elf -ex "target remote :3333" -ex "set \$x1 = 0"' C-m
 
 tmux send-keys -t "veil-debug:debug.2" \
     'until openocd -f scripts/jlink.cfg; do sleep 1; done' C-m
