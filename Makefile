02f489e7c69b7244d7c42f25bd4cddf1cb817f84
Developful <preciseful1@gmail.com>
2026-09-11 13:03:49 +0300

    jlink

--- Makefile
+++ Makefile
@@ -35,12 +35,19 @@ OBJ_FILES += $(ASM_FILES:$(SRC_DIR)/%.S=$(BUILD_DIR)/%_s.o)
 DEP_FILES = $(OBJ_FILES:%.o=%.d)
 -include $(DEP_FILES)
 
+jlink: COPS += -g
+jlink: ASMOPS += -DDEBUG_WAIT -g
+jlink: clean linker.ld $(OBJ_FILES)
+	@$(ARMGNU)-ld -pie -T linker.ld -o $(BUILD_DIR)/kernel8.elf  $(OBJ_FILES)
+	@$(ARMGNU)-objcopy $(BUILD_DIR)/kernel8.elf -O binary kernel8.img
+	scripts/debug.sh $(RPI_PATH) $(TTYDEV) $(VIEW_TTY) "jlink"
+
 debug: COPS += -g
 debug: ASMOPS += -DDEBUG_WAIT -g
 debug: clean linker.ld $(OBJ_FILES)
 	@$(ARMGNU)-ld -pie -T linker.ld -o $(BUILD_DIR)/kernel8.elf  $(OBJ_FILES)
 	@$(ARMGNU)-objcopy $(BUILD_DIR)/kernel8.elf -O binary kernel8.img
-	scripts/debug.sh $(RPI_PATH) $(TTYDEV) $(VIEW_TTY)
+	scripts/debug.sh $(RPI_PATH) $(TTYDEV) $(VIEW_TTY) "debug"
 
 kernel8.img: linker.ld $(OBJ_FILES)
 	@$(ARMGNU)-ld -pie -T linker.ld -o $(BUILD_DIR)/kernel8.elf  $(OBJ_FILES)
--- scripts/debug.sh
+++ scripts/debug.sh
@@ -9,13 +9,15 @@ tmux send-keys -t "veil-debug:debug.0" \
     "scripts/view_tty.sh $2 $3" C-m
 
 tmux split-window -v -t "veil-debug:debug.0"
-tmux split-window -h -t "veil-debug:debug.1"
 
 tmux send-keys -t "veil-debug:debug.1" \
-    'clear; until aarch64-none-elf-gdb -q -batch -ex "target remote :3333" -ex "monitor halt" build/kernel8.elf >/dev/null 2>&1; do sleep 1; done; aarch64-none-elf-gdb -q build/kernel8.elf -ex "target remote :3333" -ex "set \$x1 = 0"' C-m
-
-tmux send-keys -t "veil-debug:debug.2" \
     'until openocd -f scripts/jlink.cfg; do sleep 1; done' C-m
+
+if [[ $4 == "debug" ]]; then
+tmux split-window -h -t "veil-debug:debug.1"
+tmux send-keys -t "veil-debug:debug.2" \
+    'clear; until aarch64-none-elf-gdb -q -batch -ex "target remote :3333" -ex "monitor halt" build/kernel8.elf >/dev/null 2>&1; do sleep 1; done; aarch64-none-elf-gdb -q build/kernel8.elf -ex "target remote :3333" -ex "set \$x1 = 0"' C-m
+fi
 
 tmux select-pane -t "veil-debug:debug.1"
 tmux attach-session -t "veil-debug"
