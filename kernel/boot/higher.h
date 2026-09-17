b2b3781e377dac274b9d565344bc5a1cd703a037
Developful <preciseful1@gmail.com>
2026-09-18 01:22:11 +0300

    docs

--- kernel/boot/higher.h
+++ kernel/boot/higher.h
@@ -1,5 +1,19 @@
+/**
+ * @file
+ * @author Developful
+ * @brief Higher VA jump helpers.
+ * @date 2026-09-18
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #pragma once
 
 extern void jump_high(unsigned long pgd, unsigned long high_pgd, unsigned long last_page);
 
+/**
+ * @brief Initiate the jump to high VA.
+ *
+ * @param dtb The dtb address passed at boot.
+ */
 void JumpToHigher(void *dtb);
--- kernel/system/debug/dcc.h
+++ kernel/system/debug/dcc.h
@@ -1,5 +1,17 @@
+/**
+ * @file
+ * @author Developful
+ * @brief ARM DCC communication helpers.
+ * @date 2026-09-18
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #pragma once
 
 extern void tell_host(unsigned int operation, void *value);
 
+/**
+ * @brief Writes output through the DCC channel.
+ */
 void DebugWrite(char *fmt, ...);
